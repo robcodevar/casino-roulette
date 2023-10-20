09e072429d893a51d95cb8860e6766578a984976
bobcodevar <vargasorellanajoseroberto@gmail.com>
2023-10-20 01:34:35 -0400

    Se subio la imagen del tablero de apuestas , problema con el gif

---
+++ src/main/java/com/mycompany/roulette/Roulette.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.roulette;
+
+import com.mycompany.GUI.Table;
+
+/**
+ *
+ * @author bobvo
+ */
+public class Roulette {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Table t = new Table();
+        t.setVisible(true);
+    }
+}
